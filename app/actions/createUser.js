13b1f7057bfee867ee08eed9daa8a4b6fb5eb7f2
Bernard Lanz De Leon <acelanz@Bernards-MacBook-Air.local>
2025-03-26 20:58:36 +0800

    fix?

--- app/actions/createUser.js
+++ app/actions/createUser.js
@@ -6,41 +6,28 @@ async function createUser(previousState, formData) {
   const name = formData.get('name');
   const email = formData.get('email');
   const password = formData.get('password');
-  const confirmPassword = formData.get('confirm-password');
+  const confirmPassword = formData.get('confirmPassword'); // Fixed field name
 
   if (!email || !name || !password) {
-    return {
-      error: 'Please fill in all fields',
-    };
+    return { error: 'Please fill in all fields' };
   }
 
   if (password.length < 8) {
-    return {
-      error: 'Password must be at least 8 characters long',
-    };
+    return { error: 'Password must be at least 8 characters long' };
   }
 
   if (password !== confirmPassword) {
-    return {
-      error: 'Passwords do not match',
-    };
+    return { error: 'Passwords do not match' };
   }
 
-  // Get account instance
   const { account } = await createAdminClient();
 
   try {
-    // Create user
     await account.create(ID.unique(), email, password, name);
-
-    return {
-      success: true,
-    };
+    return { success: true };
   } catch (error) {
     console.log('Registration Error: ', error);
-    return {
-      error: 'Could not register user',
-    };
+    return { error: 'Could not register user' };
   }
 }
 
