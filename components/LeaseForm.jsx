f19fe85406f0c59488d93f94b8b3045efee6abc1
Bernard Lanz De Leon <acelanz@Bernards-MacBook-Air.local>
2025-05-13 01:30:32 +0800

    fix

--- components/LeaseForm.jsx
+++ components/LeaseForm.jsx
@@ -88,8 +88,8 @@ const BookingForm = ({ room }) => {
             name="agenda"
             rows={3}
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            placeholder="Describe your purpose for reserving the space."
-            required
+            placeholder="Describe your request."
+            
           ></textarea>
         </div>
 
